7fea83dd2e0fe74f54bf4d869e71ce9730d82128
profalexpimentel-design <profalexpimentel@gmail.com>
2026-05-09 01:10:37 -0300

    corrige leitura do body

--- api/generate-question.js
+++ api/generate-question.js
@@ -18,10 +18,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const body =
-  typeof req.body === "string"
-    ? JSON.parse(req.body)
-    : req.body;
+    const body = req.body || {};
 
 const { subject, level, difficulty } = body;
 
